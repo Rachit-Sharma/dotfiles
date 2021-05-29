c034f048c69175a3bad0f4c12dde2e6e13c1e0d4
Rachit Sharma <rachit1996@ymail.com>
2021-05-29 07:40:30 +0530

    Better line-number colour in emacs

--- home/rachit/.config/emacs/init.el
+++ home/rachit/.config/emacs/init.el
@@ -74,7 +74,7 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-one t)
-
+  (set-face-attribute 'line-number nil :foreground "#808080")
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
 
