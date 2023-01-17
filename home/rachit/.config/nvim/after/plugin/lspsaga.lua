4e5c31032ae9b622bf65ae01f0c531a2292ce0fe
Rachit Sharma <rachit.sharma@hotstar.com>
2023-01-17 13:13:28 +0530

    change lspsaga keybindings to avoid clash with lsp

--- home/rachit/.config/nvim/after/plugin/lspsaga.lua
+++ home/rachit/.config/nvim/after/plugin/lspsaga.lua
@@ -8,48 +8,54 @@ saga.setup({
 -- if there is no implement it will hide
 -- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump back
-vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+vim.keymap.set("n", "<leader>lgh", "<cmd>Lspsaga lsp_finder<CR>")
 
 -- Code action
-vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+vim.keymap.set({"n","v"}, "<leader>lca", "<cmd>Lspsaga code_action<CR>")
 
 -- Rename
-vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
+vim.keymap.set("n", "<leader>lgr", "<cmd>Lspsaga rename<CR>")
 
 -- Peek Definition
 -- you can edit the definition file in this flaotwindow
 -- also support open/vsplit/etc operation check definition_action_keys
 -- support tagstack C-t jump back
-vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+vim.keymap.set("n", "<leader>lgd", "<cmd>Lspsaga peek_definition<CR>")
 
--- Show line diagnostics
-vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+-- Show line diagnostics you can pass argument ++unfocus to make
+-- show_line_diagnostics float window unfocus
+vim.keymap.set("n", "<leader>lsl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 
--- Show cursor diagnostics
-vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+-- Show cursor diagnostic
+-- also like show_line_diagnostics  support pass ++unfocus
+vim.keymap.set("n", "<leader>lsc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+-- Show buffer diagnostic
+vim.keymap.set("n", "<leader>lsb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
 
 -- Diagnostic jump can use `<c-o>` to jump back
-vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
-vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 
--- Only jump to error
+-- Diagnostic jump with filter like Only jump to error
 vim.keymap.set("n", "[E", function()
   require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+end)
 vim.keymap.set("n", "]E", function()
   require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+end)
 
--- Outline
-vim.keymap.set("n","<leader>o", "<cmd>LSoutlineToggle<CR>",{ silent = true })
+-- Toggle Outline
+vim.keymap.set("n","<leader>lo", "<cmd>Lspsaga outline<CR>")
 
 -- Hover Doc
-vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+-- if there has no hover will have a notify no information available
+-- to disable it just Lspsaga hover_doc ++quiet
+vim.keymap.set("n", "<leader>lK", "<cmd>Lspsaga hover_doc<CR>")
+
+-- Callhierarchy
+vim.keymap.set("n", "<Leader>lci", "<cmd>Lspsaga incoming_calls<CR>")
+vim.keymap.set("n", "<Leader>lco", "<cmd>Lspsaga outgoing_calls<CR>")
 
 -- Float terminal
-vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
--- if you want to pass some cli command into a terminal you can do it like this
--- open lazygit in lspsaga float terminal
-vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>", { silent = true })
--- close floaterm
-vim.keymap.set("t", "<A-d>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
+vim.keymap.set({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
