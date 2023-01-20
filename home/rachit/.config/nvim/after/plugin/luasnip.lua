e4fa089aad614eea3b43ff7b3137532a63a5e202
Rachit Sharma <rachit.sharma@hotstar.com>
2023-01-21 00:15:40 +0530

    luasnip keybinding

--- home/rachit/.config/nvim/after/plugin/luasnip.lua
+++ home/rachit/.config/nvim/after/plugin/luasnip.lua
@@ -1,1 +1,23 @@
-require'luasnip'.filetype_extend("javascript", {"javascriptreact"})
+local ls = require("luasnip")
+
+ls.config.set_config({
+	updateevents = "TextChanged,TextChangedI",
+})
+
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
+end, { silent = true })
+
+ls.filetype_extend("javascript", { "javascriptreact" })
+
+require("luasnip.loaders.from_vscode").lazy_load({
+	paths = "~/.local/share/nvim/site/pack/packer/start/friendly-snippets/snippets/javascript/jsdoc.json",
+	include = { "javascript" },
+})
