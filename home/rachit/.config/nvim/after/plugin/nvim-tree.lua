cc9c2e99edbdd738c99a894bc69013c63dcff603
Rachit Sharma <rachit.sharma@hotstar.com>
2023-02-01 15:52:44 +0530

    feat: have nvim tree update focused file

--- home/rachit/.config/nvim/after/plugin/nvim-tree.lua
+++ home/rachit/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,8 @@
 vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle, { desc = "Toggle Nvim Tree" })
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	update_focused_file = {
+		enable = true,
+	},
+})
