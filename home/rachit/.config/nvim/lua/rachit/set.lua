045b9c49734961a5a29cd156cce87812a5d76964
Rachit Sharma <rachit.sharma@hotstar.com>
2024-01-02 12:37:26 +0530

    feat: fix fonts in MacOS

--- home/rachit/.config/nvim/lua/rachit/set.lua
+++ home/rachit/.config/nvim/lua/rachit/set.lua
@@ -34,7 +34,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- for neovide
-vim.opt.guifont = { "FantasqueSansM Nerd Font Mono:h15" }
+vim.opt.guifont = { "VictorMono Nerd Font,FantasqueSansM Nerd Font Mono:h15" }
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_no_idle = true
 vim.g.neovide_confirm_quit = true
